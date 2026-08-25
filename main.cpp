788a9ca244c670ea8ad30ee4ea390d0dd8fcbc5b
K Hashimoto <kai.hashimoto1@outlook.com>
2026-08-25 19:05:12 -0400

    grid

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include "grid.cpp"
+#include "ship.cpp"
+using namespace std;
 
 void print_help() {
-    std::cout << "Usage: my_cli [options]\n"
+    cout << "Usage: my_cli [options]\n"
               << "Options:\n"
               << "  -h, --help     Show this help message\n"
               << "  -v, --version  Show application version\n"
@@ -12,7 +15,7 @@ void print_help() {
 
 int main(int argc, char* argv[]) {
     // Safely convert raw arguments to a vector of strings
-    std::vector<std::string> args(argv + 1, argv + argc);
+    vector<string> args(argv + 1, argv + argc);
 
     if (args.empty()) {
         print_help();
@@ -25,21 +28,26 @@ int main(int argc, char* argv[]) {
             return 0;
         } 
         else if (args[i] == "-v" || args[i] == "--version") {
-            std::cout << "v1.0.0\n";
+            cout << "v1.0.0\n";
             return 0;
         } 
         else if (args[i] == "-n" || args[i] == "--name") {
             // Check if the next token exists to prevent out-of-bounds errors
             if (i + 1 < args.size()) {
-                std::string name = args[++i]; // Consume the value
-                std::cout << "Hello, " << name << "!\n";
+                string name = args[++i]; // Consume the value
+                cout << "Hello, " << name << "!\n";
             } else {
-                std::cerr << "Error: --name requires a value.\n";
+                cerr << "Error: --name requires a value.\n";
                 return 1;
             }
-        } 
+        }
+        else if (args[i] == "-g") {
+            Grid p1(1);
+            cout << p1.drawBoard();
+        }
+
         else {
-            std::cerr << "Unknown argument: " << args[i] << "\n";
+            cerr << "Unknown argument: " << args[i] << "\n";
             print_help();
             return 1;
         }
